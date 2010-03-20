f190bd1582ee0a21937513ce29e5533e6ab77b1e
Matthew <matt.chavez@integrumtech.com>
2010-03-20 11:42:46 -0700

    Initial Xcode Project commit with .gitignore

---
+++ failAppDelegate.m
@@ -0,0 +1,19 @@
+//
+//  failAppDelegate.m
+//  fail
+//
+//  Created by Matthew Chavez on 3/20/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "failAppDelegate.h"
+
+@implementation failAppDelegate
+
+@synthesize window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+	// Insert code here to initialize your application 
+}
+
+@end
---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  fail
+//
+//  Created by Matthew Chavez on 3/20/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
